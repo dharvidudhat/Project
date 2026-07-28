9426b54d3454556003565b15ad664c7a180158b9
dharvidudhat <dharvidudhat@gmail.com>
2026-07-28 16:11:20 +0530

    Add Sidebar component with navigation links

---
+++ Sidebar.php
@@ -0,0 +1,18 @@
+<div class="sidebar">
+
+    <h3>Swiffin Cake House</h3>
+
+    <a href="dashboard.php">🏠 Dashboard</a>
+
+    <a href="category.php">📂 Category</a>
+
+    <a href="cake.php">🎂 Cake</a>
+
+    <a href="orders.php">🛒 Orders</a>
+
+    <a href="users.php">👤 Users</a>
+
+    <a href="logout.php">🚪 Logout</a>
+
+</div>
+
