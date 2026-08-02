106b094e12a931d88fc6cb2a4613b9549659e634
dharvidudhat <dharvidudhat@gmail.com>
2026-08-02 15:46:18 +0530

    Create Caketable.php

---
+++ Caketable.php
@@ -0,0 +1,11 @@
+CREATE TABLE cake (
+    cake_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT NOT NULL,
+    cake_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    description TEXT,
+    image VARCHAR(255) NOT NULL,
+    status ENUM('Active','Inactive') DEFAULT 'Active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
